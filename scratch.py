a72ca98eee08ccf5b7c108d6e6f71e574949d766
Theo McCaie <tam203@gmail.com>
2020-02-12 15:14:37 +0000

    added check for data parity in scratch.py

--- scratch.py
+++ scratch.py
@@ -1,6 +1,7 @@
 from tiledb_xarray import TileDBStore, open_tiledb
 import h5py
 import xarray as xr
+import numpy as np
 
 TB_DS = "./data/local.tiledb"
 
@@ -18,3 +19,9 @@ print("--------------")
 print("\\/ netcdf \\/")
 l_ds = xr.open_dataset('./data/local.nc')
 print(l_ds)
+
+local_data = l_ds['precipitation_flux'][0:5, 0:10, 10:20].data
+tiledb_data = t_ds['precipitation_flux'][0:5, 0:10, 10:20].data
+
+assert np.all(local_data == tiledb_data)
+print("*** Yay data match! ***")
